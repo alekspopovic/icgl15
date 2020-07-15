b75ee0765d17b6e852c86e62a2eab266f32aa952
Aleks Popovic <aleksandar88popovic@gmail.com>
2020-07-15 21:20:45 +0200

    404 page fix

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,42 +1,16 @@
 import React from "react"
-import { graphql } from "gatsby"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import fourOhFourStyles from "../styles/404.module.css"
-// import image from "../assets/404.svg"
+import Button from "../components/button"
+import pageNotFoundStyles from "../styles/404.module.css"
 
 class NotFoundPage extends React.Component {
   render() {
-    const headerText = "404"
-    const subHeaderText = "Page Not Found"
-
     return (
-      <Layout
-        headerText={headerText}
-        subHeaderText={subHeaderText}
-        hideHeader={true}
-        location={this.props.location}
-      >
-        <SEO title="404: Not Found" />
-        <div className={fourOhFourStyles.container}>
-          <div className={fourOhFourStyles.imageContainer}>
-            {/* <img src={image} alt="page not found" /> */}
-          </div>
-          <div className={fourOhFourStyles.content}>404: page not found</div>
-        </div>
-      </Layout>
+      <div className={pageNotFoundStyles.container}>
+        <h1>404: PAGE NOT FOUND</h1>
+        <Button isInternal={true} url="/" text="Return to home page" />
+      </div>
     )
   }
 }
 
 export default NotFoundPage
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
