3c3c04f94cd112175f595991a1651f006b34f598
Aleks Popovic <aleksandar88popovic@gmail.com>
2020-07-18 23:46:12 +0200

    Removing text from home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,27 +13,12 @@ class CallForPapers extends React.Component {
       >
         <SEO title="ICGL15" pagePath={this.props.location.pathname} />
         <div className={contentStyles.content}>
-          <h2>16-19 September 2021</h2>
-          <h3>1st Call for papers</h3>
           <p>Dear Colleagues,</p>
           <p>
             The Department of Modern Greek Studies, Faculty of Philology,
             University of Belgrade, is pleased to announce the 15th
             International Conference on Greek Linguistics, which will be held in
             Belgrade, <strong>16-19 September 2021</strong>.
-          </p>
-          <p>
-            ICGL is a biennial meeting, held since 1993, that focuses on all
-            aspects of the linguistic study and analysis of Greek.
-          </p>
-          <p>
-            The conference invites contributions to any field of Greek
-            Linguistics (including, but not limited to: applied linguistics,
-            computational linguistics, corpus linguistics, dialectology,
-            discourse analysis, historical linguistics, language teaching,
-            lexicography, lexicology, morphology, neurolinguistics, phonetics,
-            phonology, pragmatics, psycholinguistics, semantics,
-            sociolinguistics, syntax).
           </p>
           <hr />
         </div>
