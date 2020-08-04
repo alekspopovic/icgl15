ac158a702f49757ef0ac5ed0cead44114f3050e0
Aleks Popovic <aleksandar88popovic@gmail.com>
2020-08-04 23:42:39 +0200

    Registration content

--- src/pages/registration.js
+++ src/pages/registration.js
@@ -73,6 +73,58 @@ class Registration extends React.Component {
             text="Download Payment Instructions"
             isCentered={true}
           />
+
+          <h2>Participation package</h2>
+          <p>
+            The fee includes: conference attendance, the conference bag and
+            inclusions (book of abstracts, program etc.), participation in
+            coffee breaks and light meals, sightseeing tours of Belgrade and
+            publication of the presented papers in the printed or electronic
+            proceedings (after the peer review process).
+          </p>
+
+          <h2>Please note the following</h2>
+          <ul>
+            <li>
+              All presenters (authors) at the conference must register by the
+              30th June.
+            </li>
+            <li>
+              For each oral / poster presentation, at least 1 author must
+              register.
+            </li>
+            <li>
+              Each author can register only for 1 presentation, even if he / she
+              is co-authoring 2 presentations.
+            </li>
+            <li>
+              If none of the authors of an oral / poster presentation has
+              registered by the 30th of June, their presentation will not be
+              included in the program of the conference.{" "}
+            </li>
+          </ul>
+
+          <h2>Registration Confirmation</h2>
+          <p>
+            After completing the payment process, the participants are kindly
+            requested to send the payment proof and their personal information
+            (name, affiliation, contact phone number, email) to the following
+            address:{" "}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="mailto:icgl15@fil.bg.ac.rs"
+            >
+              icgl15@fil.bg.ac.rs
+            </a>
+          </p>
+          <p>
+            Participants will be sent a final confirmation of registration via
+            email. Confirmation will take place after having received the
+            payment proof. In case no confirmation email (within a few days) is
+            sent back, please contact the secretarial support. Participants are
+            kindly requested to state a valid email address.
+          </p>
         </div>
       </Layout>
     )
